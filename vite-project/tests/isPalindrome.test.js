e1d13faa499bf9bd5c6c4a780b6caec2a6a7be14
Mélanie <117924021+MelB04@users.noreply.github.com>
2024-03-08 10:29:46 +0100

    isPalindrome

---
+++ vite-project/tests/isPalindrome.test.js
@@ -0,0 +1,35 @@
+import { isPalindrome } from "../functions/isPalindrome.js";
+import { expect, test } from "vitest";
+
+test("isPalindrome True", () => {
+  expect(isPalindrome("coloc")).toBe(true);
+});
+
+test("isPalindrome False", () => {
+  expect(isPalindrome("melanie")).toBe(false);
+});
+
+test("isPalindrome False", () => {
+  expect(isPalindrome("Et six... existe ")).toBe(true);
+});
+
+test.each(["elle", "non", "coloc"])("test each", (item) => {
+  expect(isPalindrome(item)).toBe(true);
+});
+
+test("isPalindrome number in str", () => {
+  expect(isPalindrome("9559")).toBe(true);
+});
+
+test("isPalindrome accents", () => {
+  expect(isPalindrome("semâmes")).toBe(true);
+});
+
+test("isPalindrome virgules points", () => {
+  expect(isPalindrome("Engage le jeu, que je le gagne.")).toBe(true);
+});
+
+test("isPalindrome rien", () => {
+  expect(isPalindrome("")).toBe(true);
+});
+
