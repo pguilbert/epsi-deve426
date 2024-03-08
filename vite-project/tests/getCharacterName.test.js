f422fb2a8f90b98fa45e5df2d31d4f3e86802c58
Mélanie <117924021+MelB04@users.noreply.github.com>
2024-03-08 14:49:38 +0100

    getProduct test1

--- vite-project/tests/getCharacterName.test.js
+++
@@ -1,8 +0,0 @@
-import { getCharacterName } from "../functions/getCharacterName";
-import { describe, expect, test } from "vitest";
-
-describe("getCharacterName", () => {
-    test.todo("");
-    test.todo("");
-  });
-  
