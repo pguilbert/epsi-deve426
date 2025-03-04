3d0aeb68e62ff08d587f43eed5eec4f4612662b1
Halaz1a <walaskaa.pro@gmail.com>
2025-03-04 12:28:26 +0100

    Premiers tests Playwright

---
+++ vite-project/e2e/counter.spec.js
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+    await page.goto('http://localhost:5173/');
+  
+    await expect(page).toHaveTitle(/Vite App/);
+    await page.getByRole('button').click();
+  });
