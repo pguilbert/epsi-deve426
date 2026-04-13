a6fec634265a5882434c7af904d3bf0b1918c46d
Salingue Clément <clement.salingue@ecoles-epsi.net>
2026-04-13 14:59:31 +0200

    jsdom

---
+++ vite-project/tests/SetupCounter.test.js
@@ -0,0 +1,22 @@
+import { describe, it, expect } from "vitest";
+import { setupCounter } from "../functions/setupCounter.js";
+
+describe("setupCounter", () => {
+  it("met 0 au début", () => {
+    const element = document.createElement("button");
+
+    setupCounter(element);
+
+    expect(element.innerHTML).toBe("count is 0");
+  });
+
+  it("augmente quand on clique", () => {
+    const element = document.createElement("button");
+
+    setupCounter(element);
+
+    element.click();
+
+    expect(element.innerHTML).toBe("count is 1");
+  });
+});
