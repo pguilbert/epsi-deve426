81f281520c655e9f04ad79caa2bc06fb477d534f
Kaelian Baudelet <aconique@gmail.com>
2026-04-13 15:00:54 +0200

    fix: update setupCounter.test.js to add jsdom

--- vite-project/tests/setupCounter.test.js
+++ vite-project/tests/setupCounter.test.js
@@ -1,16 +1,10 @@
-import { expect, test, vi } from "vitest";
+//* rather than set environment: "jsdom" in vitest.config.js we say to vitest to allow jsdom in this test when he read this file */
+/** @vitest-environment jsdom */
+import { expect, test } from "vitest";
 import { setupCounter } from "../functions/setupCounter";
 
 test("setupCounter (should increment the counter when clicked)", () => {
-  let clickCallback;
-  const element = {
-    innerHTML: "",
-    addEventListener: vi.fn((event, callback) => {
-      if (event === "click") {
-        clickCallback = callback;
-      }
-    }),
-  };
+  const element = document.createElement("button");
 
   setupCounter(element);
 
@@ -18,36 +12,22 @@ test("setupCounter (should increment the counter when clicked)", () => {
   expect(element.innerHTML).toBe("count is 0");
 
   // trigger click
-  if (clickCallback) {
-    clickCallback();
-  }
+  element.click();
   expect(element.innerHTML).toBe("count is 1");
 
   // trigger click again....
-  if (clickCallback) {
-    clickCallback();
-  }
+  element.click();
   expect(element.innerHTML).toBe("count is 2");
 });
 
 test("setupCounter (should clamp the counter at 10)", () => {
-  let clickCallback;
-  const element = {
-    innerHTML: "",
-    addEventListener: vi.fn((event, callback) => {
-      if (event === "click") {
-        clickCallback = callback;
-      }
-    }),
-  };
+  const element = document.createElement("button");
 
   setupCounter(element);
 
   // click 12 times to test if its works
-  if (clickCallback) {
-    for (let i = 0; i < 12; i++) {
-      clickCallback();
-    }
+  for (let i = 0; i < 12; i++) {
+    element.click();
   }
 
   expect(element.innerHTML).toBe("count is 10");
